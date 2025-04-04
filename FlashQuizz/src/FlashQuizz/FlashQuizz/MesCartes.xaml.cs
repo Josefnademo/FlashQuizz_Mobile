2196bdd501f589ea84ac6298a27f5d39c93ef81c
YOSEF NADEMO <72689921+Josefnademo@users.noreply.github.com>
2025-04-04 13:35:55 +0200

    maquette(fig), images(logo), CDC, code(navigation, mvvm)

---
+++ FlashQuizz/src/FlashQuizz/FlashQuizz/MesCartes.xaml.cs
@@ -0,0 +1,9 @@
+namespace FlashQuizz;
+
+public partial class MesCartes : ContentPage
+{
+	public MesCartes()
+	{
+		InitializeComponent();
+	}
+}
---
+++ FlashQuizz/src/FlashQuizz/FlashQuizz/ViewModels/Mvvm1ViewModel.cs
@@ -0,0 +1,22 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlashQuizz.ViewModels
+{
+    public partial class Mvvm1ViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private int counter = 0;
+
+        [RelayCommand]
+        private void Increment(int incrementValue)
+        {
+            Counter += incrementValue;
+        }
+    }
+}
